399e3b9d8df490e43e1bb6f0c80af7d39bca594b
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-25 17:39:47 +0300

    modify navigation view with navigtationbar title and navigation bar items such a button and image

--- Topics/NavigationView/NavigationView/ContentView.swift
+++ Topics/NavigationView/NavigationView/ContentView.swift
@@ -11,15 +11,11 @@ struct ContentView: View {
     var body: some View {
        
         // For simpler layouts navigation views should be the top-level thing in your view, but if you’re using them inside a TabView then the navigation view should be inside the tab view.
-        
-
         NavigationView {
             Text("Hello, World")
                 .navigationTitle("Navigation") // navigationTitle() modifier belongs to the text view, not to the navigation view
                 .navigationBarTitleDisplayMode(.inline)
-
         }
-
     }
 }
 
