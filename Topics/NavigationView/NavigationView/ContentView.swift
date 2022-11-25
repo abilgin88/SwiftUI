ae78f5a73a46fa990bf2cf0543a9eabcd5b38fd1
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-25 16:39:30 +0300

    Getting a basic NavigationView with a title

---
+++ Topics/NavigationView/NavigationView/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  NavigationView
+//
+//  Created by Abdullah Bilgin on 11/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+        // For simpler layouts navigation views should be the top-level thing in your view, but if you’re using them inside a TabView then the navigation view should be inside the tab view.
+        
+
+        NavigationView {
+            Text("Hello, World")
+                .navigationTitle("Navigation") // navigationTitle() modifier belongs to the text view, not to the navigation view
+                .navigationBarTitleDisplayMode(.inline)
+
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Topics/NavigationView/NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by Abdullah Bilgin on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
