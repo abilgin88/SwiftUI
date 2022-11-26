777dc235f692e80b638750dd102776deb3fb50ec
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-26 08:41:42 +0300

    Building out the ui for our counter

---
+++ Topics/SimpleMVVM/SimpleMVVM/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  SimpleMVVM
+//
+//  Created by Abdullah Bilgin on 11/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("[Count]")
+                .font(.system(size: 50, weight: .bold, design: .rounded))
+                .padding()
+            Button("increase") {
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Topics/SimpleMVVM/SimpleMVVM/SimpleMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMVVMApp.swift
+//  SimpleMVVM
+//
+//  Created by Abdullah Bilgin on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
