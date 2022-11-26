9fa581a4b81057a00d334aa68ebfda226f59ca3c
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-26 10:18:28 +0300

    Simple MVVM -> Build View Model for single View (ContentView) and link to ContentView

--- Topics/SimpleMVVM/SimpleMVVM/ContentView.swift
+++ Topics/SimpleMVVM/SimpleMVVM/ContentView.swift
@@ -6,16 +6,33 @@
 //
 
 import SwiftUI
+
+// 2: Building View Model
+struct Counter { // Why do not use ":ObservedObject"?
+
+    var value : Int = 0
+    
+    mutating func increase() { // mutating?
+        value += 1
+    }
+}
+
 struct ContentView: View {
+    
+    // 3: create state object: ContentView SOURCE -> where going to read updates from
+   @State private var counter = Counter()
+    
     var body: some View {
         
         // 1: Building out the UI for our Counter
         VStack {
-            Text("[Count]")
+            // 5: link content.value from struct to ContentView -> Text
+            Text("\(counter.value)") // use string interpolation here because "value" is an integer so we can not directly pass it
                 .font(.system(size: 50, weight: .bold, design: .rounded))
                 .padding()
             Button("increase") {
-                
+                // 4: use increase function
+                counter.increase()
             }
             .buttonStyle(.borderedProminent)
             .controlSize(.large)
