bac2ba460800f71d2a8a266babd408b1e7d1ff60
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-27 20:37:10 +0300

    Project: Split Bill Together: Creating Project File

---
+++ Projects/SplitBillTogether/SplitBillTogether/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SplitBillTogether
+//
+//  Created by Abdullah Bilgin on 11/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/SplitBillTogether/SplitBillTogether/SplitBillTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillTogetherApp.swift
+//  SplitBillTogether
+//
+//  Created by Abdullah Bilgin on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
